86dd1aad54440c80788457a9c9baf134139a40b6
Add00w <jiincade@gmail.com>
2023-07-15 16:46:06 +0300

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Abdullahi Addow on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
